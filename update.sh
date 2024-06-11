5d7f0b7f84e02f51f192628fbcb11b308753b430
Wilton Rodrigues <wiltonsr94@gmail.com>
2024-06-11 16:39:18 -0300

    Applying code style

--- update.sh
+++ update.sh
@@ -2,20 +2,20 @@
 set -eo pipefail
 
 variants=(
-    open
-    blacklist
-    whitelist
+  open
+  blacklist
+  whitelist
 )
 
 function create_variant() {
-    variant=$1
-    variantFile="$variant.Dockerfile"
+  variant=$1
+  variantFile="$variant.Dockerfile"
 
-    touch "${variantFile}"
+  touch "${variantFile}"
 
-    template="Dockerfile.template"
+  template="Dockerfile.template"
 
-    cat <<__EOF__ >"${variantFile}"
+  cat <<__EOF__ >"${variantFile}"
 #
 # NOTE: THIS DOCKERFILE IS GENERATED VIA update.sh from ${template}
 #
@@ -23,34 +23,34 @@ function create_variant() {
 #
 __EOF__
 
-    cat "$template" >>"$variantFile"
+  cat "$template" >>"$variantFile"
 
-    echo "updating $variantFile"
+  echo "updating $variantFile"
 
-    case "$variant" in
-    open)
-        cat << '__EOF__' > /tmp/makefileSedExpressions
+  case "$variant" in
+  open)
+    cat <<'__EOF__' >/tmp/makefileSedExpressions
       # No need to change Makefile to open mode
 __EOF__
 
-        cat << '__EOF__' > /tmp/opentrackerSedExpressions
+    cat <<'__EOF__' >/tmp/opentrackerSedExpressions
     # Opentrack conf whitelist sed expressions
     sed -ri -e '\
       s!(.*)(tracker.user)(.*)!\2 opentracker!g; \
     ' /tmp/stage/etc/opentracker/opentracker.conf ; \
 __EOF__
 
-        ;;
+    ;;
 
-    blacklist)
-        cat << '__EOF__' > /tmp/makefileSedExpressions
+  blacklist)
+    cat <<'__EOF__' >/tmp/makefileSedExpressions
       # Makefile blacklist sed expressions
       sed -ri -e '\
         /^#.*DWANT_ACCESSLIST_BLACK/s/^#//; \
       ' Makefile ; \
 __EOF__
 
-        cat << '__EOF__' > /tmp/opentrackerSedExpressions
+    cat <<'__EOF__' >/tmp/opentrackerSedExpressions
       # Opentrack conf blacklist sed expressions
       sed -ri -e '\
         s!(.*)(tracker.user)(.*)!\2 opentracker!g; \
@@ -59,17 +59,17 @@ __EOF__
       touch /tmp/stage/etc/opentracker/blacklist ; \
 __EOF__
 
-        ;;
+    ;;
 
-    whitelist)
-        cat << '__EOF__' > /tmp/makefileSedExpressions
+  whitelist)
+    cat <<'__EOF__' >/tmp/makefileSedExpressions
       # Makefile whitelist sed expressions
       sed -ri -e '\
         /^#.*DWANT_ACCESSLIST_WHITE/s/^#//; \
       ' Makefile ; \
 __EOF__
 
-        cat << '__EOF__' > /tmp/opentrackerSedExpressions
+    cat <<'__EOF__' >/tmp/opentrackerSedExpressions
       # Opentrack conf whitelist sed expressions
       sed -ri -e '\
         s!(.*)(tracker.user)(.*)!\2 opentracker!g; \
@@ -78,16 +78,16 @@ __EOF__
       touch /tmp/stage/etc/opentracker/whitelist ; \
 __EOF__
 
-        ;;
-    esac
+    ;;
+  esac
 
-    # Replace the variables.
-    sed -i '/%%MAKEFILE_SED_EXPRESSIONS%%/ {
+  # Replace the variables.
+  sed -i '/%%MAKEFILE_SED_EXPRESSIONS%%/ {
     r /tmp/makefileSedExpressions
     d
     }' "$variantFile"
 
-    sed -i '/%%OPENTRACKER_CONF_SED_EXPRESSIONS%%/ {
+  sed -i '/%%OPENTRACKER_CONF_SED_EXPRESSIONS%%/ {
     r /tmp/opentrackerSedExpressions
     d
     }' "$variantFile"
@@ -95,12 +95,12 @@ __EOF__
 }
 
 if [ -z "$1" ]; then
-    for variant in "${variants[@]}"; do
-        create_variant "$variant"
-    done
+  for variant in "${variants[@]}"; do
+    create_variant "$variant"
+  done
 elif [[ ${variants[*]} =~ $1 ]]; then
-    create_variant "$1"
+  create_variant "$1"
 else
-    echo "Value ${1} Invalid!"
-    exit 1
+  echo "Value ${1} Invalid!"
+  exit 1
 fi
